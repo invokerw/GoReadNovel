6b75e95fef6d11b2bb75666193610ce3b7ed3be9
境界的彼方 <784856034@qq.com>
2017-04-09 23:08:56 +0800

    add  python

---
+++ spider/python/getTopByTypeNoteList.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+import urllib2
+import sys
+import re
+
+reload(sys)
+sys.setdefaultencoding('gbk')
+
+url = 'http://www.huanyue123.com/book/quanbu/allvisit-0-0-0-0-0-0-1.html'
+user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'# 将user_agent写入头信息
+headers = { 'User-Agent' : user_agent }
+try:
+    request = urllib2.Request(url,headers=headers)
+    response = urllib2.urlopen(request)
+    content = response.read().decode('gbk')
+    head = response.info()
+    # print content
+
+    # page = re.compile('<li>.*?<a.*?href="(.*?)">(.*?)</a>*?</li>', re.S)
+    page = re.compile(
+        '<dl>.*?<dt>.*?</dt>.*?<dd>.*?<h3>.*?<span.*?"uptime">(.*?)</span>.*?'
+        '<a.*?href="(.*?)".*?>(.*?)</a>.*?</h3>.*?</dd>.*?<span>(.*?)</span>.*?'
+        '<dd.*?>(.*?)</dd>.*?<dd.*?<a.*?href="(.*?)".*?>(.*?)</a>.*?</dd>.*?'
+        '</dl>', re.S)
+    # item[1] 小说地址, item[2] 小说名字, item[5]最新章节地址(可用),
+    # item[6]最新章节名字, item[3] 作者, item[0]更新时间, item[4]描述
+    hrefList = re.findall(page, content.encode('utf8'))
+
+    chapterQty = 0
+    # print "Begin->"
+    retStr = ""
+    for item in hrefList:
+        chapterQty = chapterQty + 1
+        retStr = retStr + str(chapterQty) + "--" + item[0]
+        retStr = retStr + "--" + item[1] + "--" + item[2]
+        retStr = retStr + "--" + item[3] + "--" + item[4]
+        retStr = retStr + "--" + item[5] + "--" + item[4]
+        retStr = retStr +","
+        #print item[0], item[1], item[2], item[3], item[4], item[5], item[6]
+    print retStr
+
+
+except urllib2.URLError, e:
+    if hasattr(e, "code"):
+        print e.code
+    if hasattr(e, "reason"):
+        print e.reason
---
+++ spider/python/notice.rd
@@ -0,0 +1,42 @@
+小说爬取网址
+
+http://www.huanyue123.com/book/quanbu/allvisit-0-0-0-0-0-0-1.html
+
+quanbu    全部
+
+xuanhuan  玄幻
+
+xainxia   仙侠
+
+junshi    军史
+
+kehuan    科幻
+
+lingyi    灵异
+
+yanqing   言情
+
+qita	  其他
+
+
+allvisit-0-0-0-0-0-0-1.html     第一页
+allvisit-0-0-0-0-0-0-2.html     第二页
+
+            0 1 2 3 4 5
+1-------字母 不限 a b c d 
+2-------字数  全部  30  30-50  50-100  100-200 200  
+3-------
+4-------
+5-------状态  不限 连载 完本
+6-------顺序 倒序 正序
+7-------页数
+
+
+默认    default
+
+总点击  allvote
+
+总收藏  goodnum
+
+
+
