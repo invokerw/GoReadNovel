51063236ea6890afd82ba6774fffcf520b884f1d
一方通行 <784856034@qq.com>
2017-04-13 18:44:19 +0800

    fix

--- gitpush.sh
+++ gitpush.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 
 
-mysqldump -uroot -pweifei novel > /root/gopro/src/GoReadNovel/novel.sql
 
-git status
+if [ ! -n "$1" ] ;then
+	echo "you have not input a commit word!"
+	exit 1
+else
+	echo "the commit you input is $1"
+	
+	echo "Output Database Novel"
+
+	mysqldump -uroot -pweifei novel > /root/gopro/src/GoReadNovel/novel.sql
+
+	echo "OK"
+	
+	git status
+	
+	git add -A
+
+	git commit -m "$1"
+
+	git push
+fi
+
+exit 0
 
 
-git add -A
-
-
-git commit -m "$1"
-
-
-git push
