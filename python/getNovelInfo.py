0bfc1c3e6e9efcf0e44ef363eda49c1c238300e2
一方通行 <784856034@qq.com>
2017-04-14 18:02:32 +0800

    update data

--- python/getNovelInfo.py
+++ python/getNovelInfo.py
@@ -25,82 +25,29 @@ headers = { 'User-Agent' : user_agent }
 
 try:
     request = urllib2.Request(url, headers=headers)
-    response = urllib2.urlopen(request)
+    response = urllib2.urlopen(request,timeout=10)
     content = response.read().decode('gbk')
     head = response.info()
-
-    page = re.compile('<li>.*?<a.*?href="(.*?)">(.*?)</a>*?</li>', re.S)
-
-    hrefList = re.findall(page, content.encode('utf8'))
-    chapterQty = 0
+    # print content.encode('utf8')
+	# 描述 类型 最新章节名称 最新章节url 小说状态
+    page = re.compile('<div.*?class="title">.*?<b>.*?</a>.*?<a.*?>(.*?)</a>.*?<div.*?"options".*?<span.*?'
+		'<span.*?"item">(.*?)</span>.*?<h3.*?bookinfo_intro.*?</strong>(.*?)<strong>.*?</h3>', re.S)
+    replaceBr = re.compile('<br.*?>')
+    replaceSpace = re.compile('&nbsp;')
+    info = re.findall(page, content.encode('utf8'))
     # print "Begin->"
+    # print info
     retStr = ""
-    for item in hrefList:
-        chapterQty = chapterQty + 1
-        retStr = retStr + str(chapterQty) + "-" + item[0]
-        retStr = retStr + "-" + item[1] + ","
+    for item in info:
+        strTmp = re.sub(replaceBr, "", item[2])
+        strTmp = re.sub(replaceSpace, "", strTmp)
+        retStr = retStr + item[0] + "-"
+        retStr = retStr + item[1] + "-"
+        retStr = retStr + strTmp
         # print str(chapterQty) + ":" + item
         # os.system('python GetNovelTxt.py ' + str(item))
         # print item
     print retStr
-    # 如果 chapterQty = 0 那么说明了并没有你搜的这个小说
-    # print chapterQty
-    # print "End->"
-
-except urllib2.URLError, e:
-    if hasattr(e, "code"):
-        print e.code
-    if hasattr(e, "reason"):
-        print e.reason
-#!/usr/bin/python
-# -*- coding:utf-8 -*-
-
-import urllib2
-import sys
-import re
-
-reload(sys)
-sys.setdefaultencoding('gbk')
-
-# 通过更换strs的不同请求搜索不同的书籍 可以找到对应网页的网址
-if len(sys.argv) != 2:
-    print 'Err : GetNovelTxt参数不够'
-    exit()
-url = unicode(sys.argv[1], "UTF-8")
-# strs = u'圣墟'  # 一定要有这个 u  没有u的话请求的编码会有错误
-
-# url = 'http://www.huanyue123.com/modules/article/search.php'
-
-# values = {'searchkey': strs}
-
-# data = urllib.urlencode(values)
-user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'# 将user_agent写入头信息
-headers = { 'User-Agent' : user_agent }
-
-try:
-    request = urllib2.Request(url, headers=headers)
-    response = urllib2.urlopen(request)
-    content = response.read().decode('gbk')
-    head = response.info()
-    # print content
-
-
-    page = re.compile('<li>.*?<a.*?href="(.*?)">(.*?)</a>*?</li>', re.S)
-
-    bookInfo = re.findall(page, content.encode('utf8'))
-    chapterQty = 0
-    # print "Begin->"
-    retStr = ""
-    for item in bookInfo:
-        chapterQty = chapterQty + 1
-        retStr = retStr + str(chapterQty) + "-" + item[0]
-        retStr = retStr + "-" + item[1] + ","
-        # print str(chapterQty) + ":" + item
-        # os.system('python GetNovelTxt.py ' + str(item))
-        # print item
-    print retStr
-    # 如果 chapterQty = 0 那么说明了并没有你搜的这个小说
-    # print chapterQty
     # print "End->"
 
 except urllib2.URLError, e:
